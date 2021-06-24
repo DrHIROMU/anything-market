9224e0f10a5e3500c2e48df1addedf7b66061c1c
ADVANTECH\Tommy12.Lin <drhiromu@gmail.com>
2021-06-24 13:43:50 +0800

    測試ThreadPoolTaskExecutor

---
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.tommykhlin.marketservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
--- back-end/marketservice/src/main/java/com/tommykhlin/marketservice/order/controller/CommodityController.java
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/order/controller/CommodityController.java
@@ -2,7 +2,11 @@ package com.tommykhlin.marketservice.order.controller;
 
 import com.tommykhlin.marketservice.order.model.Commodity;
 import com.tommykhlin.marketservice.order.repository.CommodityRepository;
+import com.tommykhlin.marketservice.order.service.ThreadTask;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,6 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class CommodityController {
     @Autowired
     CommodityRepository commodityRepository;
+
+    @Autowired
+    ThreadPoolTaskExecutor executor;
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @PostMapping(value = "commodity")
     public Commodity addCommodity(@RequestBody Commodity commodity){
@@ -22,5 +32,12 @@ public class CommodityController {
     public Commodity getCommodity(@PathVariable Long id){
         Commodity commodity = commodityRepository.findById(id).orElse(null);
         return  commodity;
+    }
+
+    @PostMapping(value = "thread-test")
+    public void threadTest(@RequestBody String msg){
+        for(int i=0; i<10; i++){
+            executor.execute(applicationContext.getBean(ThreadTask.class, msg+i));
+        }
     }
 }
