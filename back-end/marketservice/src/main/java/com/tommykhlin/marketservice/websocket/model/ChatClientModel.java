b04042f666e5c490a74690751c570fffefeea304
ADVANTECH\Tommy12.Lin <drhiromu@gmail.com>
2021-07-02 09:54:17 +0800

    chat_room

---
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/websocket/model/ChatClientModel.java
@@ -0,0 +1,13 @@
+package com.tommykhlin.marketservice.websocket.model;
+
+public class ChatClientModel {
+    private String Clientname;
+
+    public String getClientname() {
+        return Clientname;
+    }
+
+    public void setClientname(String clientname) {
+        Clientname = clientname;
+    }
+}
