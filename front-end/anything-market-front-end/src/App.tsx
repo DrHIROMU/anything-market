4d895f42048b1fa9f05148722b46a5c0d7d8f5bd
DrHIROMU <drhiromu@gmail.com>
2022-01-15 12:00:15 +0800

    init

---
+++ front-end/anything-market-front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import NavBar from './component/nav-bar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>  
+    </div>
+  );
+}
+
+export default App;
