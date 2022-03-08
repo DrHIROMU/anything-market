b8922de66d1b576b33ad82fff55b4cedc3f09d4f
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-03-08 13:39:39 +0800

    modify

--- back-end/marketservice/src/main/java/com/tommykhlin/marketservice/config/RedisConfig.java
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/config/RedisConfig.java
@@ -7,13 +7,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-@Configuration
-@EnableCaching
-public class RedisConfig extends CachingConfigurerSupport {
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
-        redisTemplate.setConnectionFactory(factory);
-        return redisTemplate;
-    }
+//@Configuration
+//@EnableCaching
+//public class RedisConfig extends CachingConfigurerSupport {
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+//        redisTemplate.setConnectionFactory(factory);
+//        return redisTemplate;
+//    }
+//}
+
+public class RedisConfig{
+
 }
---
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/file/controller/FileController.java
@@ -0,0 +1,30 @@
+package com.tommykhlin.marketservice.file.controller;
+
+import com.tommykhlin.marketservice.file.service.FileService;
+import com.tommykhlin.marketservice.file.vo.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileController {
+    @Autowired
+    FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            fileService.save(file);
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+}
