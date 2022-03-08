7363a8a26bdda76875c62df6ab825b9dea0d2974
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-03-08 10:56:10 +0800

    merge

--- back-end/marketservice/src/main/java/com/tommykhlin/marketservice/MarketServiceApplication.java
+++ back-end/marketservice/src/main/java/com/tommykhlin/marketservice/MarketServiceApplication.java
@@ -2,6 +2,8 @@ package com.tommykhlin.marketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+import org.springframework.web.socket.config.annotation.WebMvcStompWebSocketEndpointRegistration;
 
 @SpringBootApplication
 public class MarketServiceApplication {
