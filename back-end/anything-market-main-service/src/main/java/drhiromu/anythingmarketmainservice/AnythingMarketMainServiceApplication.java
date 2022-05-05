61b554d7c45a1a2155f81c2601f24e692f1fede2
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-05-05 09:41:11 +0800

    update

--- back-end/anything-market-main-service/src/main/java/drhiromu/anythingmarketmainservice/AnythingMarketMainServiceApplication.java
+++ back-end/anything-market-main-service/src/main/java/drhiromu/anythingmarketmainservice/AnythingMarketMainServiceApplication.java
@@ -1,13 +1,24 @@
 package drhiromu.anythingmarketmainservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnythingMarketMainServiceApplication {
+	private static final Logger log = LoggerFactory.getLogger(AnythingMarketMainServiceApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnythingMarketMainServiceApplication.class, args);
+
+		log.debug(
+				"\n----------------------------------------------------------------------\n\t" +
+						"Application '{}' is running!\n" +
+						"----------------------------------------------------------------------",
+				AnythingMarketMainServiceApplication.class.getSimpleName()
+		);
 	}
 
 }
